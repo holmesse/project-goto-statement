97c9c63349447db1f04ab3b40fbacd3085ac8f02
David Slusher <davidslusher1@hotmail.com>
2018-10-15 18:31:06 -0400

    pushing final feature01main

--- client/src/main/java/edu/wofford/wordoff/Feature01Main.java
+++ client/src/main/java/edu/wofford/wordoff/Feature01Main.java
@@ -5,12 +5,18 @@ public class Feature01Main {
     public static void main(String[] args) {
       //do if int passed
       //handle null pointer exception
+      Anagrams anagrams = new Anagrams();
       try{
         int numOfAnagrams = Integer.parseInt(args[0]);
+        List<String> anagramsOfWord = anagrams.getNumberOfAnagrams(numOfAnagrams);
+        if(anagramsOfWord != null) {
+          for(int i = 0; i < anagramsOfWord.size(); i++){
+            System.out.println(anagramsOfWord.get(i));
+          }
+        }
       }
       catch(NumberFormatException e){
           String word = args[0];
-          Anagrams anagrams = new Anagrams();
           List<String> anagrams_of_word = anagrams.getAnagramsOfWord(word);
           if(anagrams_of_word != null){
             for(int i = 0; i < anagrams_of_word.size(); i++){
