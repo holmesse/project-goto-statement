6897af8b2fb51bfb7a05654aacdb290cc1454624
Aaron Sykes <absykes1997@gmail.com>
2018-10-17 12:28:41 -0400

    Finished documenting Anagrams class

--- client/src/main/java/edu/wofford/wordoff/Anagrams.java
+++ client/src/main/java/edu/wofford/wordoff/Anagrams.java
@@ -4,12 +4,28 @@ import java.util.*;
 import java.io.*;
 import java.util.Scanner;
 
+/**
+* <h1>Anagrams</h1>
+* The Anagrams class uses a {@code Map<String, List<String>>} structure
+* to hash words in the words file according to the characters of each word
+* sorted alphabetically. This will result in a mapping of a string of
+* sorted letters to a list of words from the words file that are arrangements
+* of those letters, resulting in all words in one list being anagrams
+* of the others.
+*/
 public class Anagrams {
 
 	private Map<String, List<String>> anagrams;
 	private List<String> wordList;
 	private Random randomGenerator;
 
+	/**
+	* <h2>Default Constructor</h2>
+	* The default constructor initilizes {@code anagrams} to a HashMap
+	* of types {@code <String, List<String>>} and {@code wordList} is
+	* set as an empty {@code ArrayList<>}. The {@code randomGenerator}
+	* is initialized, and the {@link buildMapOfAnagrams} method is called.
+	*/
 	public Anagrams() {
 		anagrams = new HashMap<String, List<String>>();
 		wordList = new ArrayList<>();
@@ -17,6 +33,13 @@ public class Anagrams {
 		buildMapOfAnagrams();
 	}
 
+	/**
+	* <h2>Constructor with Random Seed</h2>
+	* This constructor is used for testing purposes or when a random
+	* number seed has already been provided.
+	* 
+	* @param seedForRandomGenerator The seed for the random generation.
+	*/
 	public Anagrams(long seedForRandomGenerator) {
 		anagrams = new HashMap<String, List<String>>();
 		wordList = new ArrayList<>();
@@ -24,30 +47,34 @@ public class Anagrams {
 		buildMapOfAnagrams();
 	}
 
+	/**
+	* <h2>Create anagrams data structure</h2>
+	* This method reads the words from the allwords.txt file and
+	* hashes them into {@code anagrams}.
+	*/
 	private void buildMapOfAnagrams() {
 		InputStream stream = getClass().getClassLoader().getResourceAsStream("allwords.txt");
-		try {
-			Scanner scanner = new Scanner(stream);
+		Scanner scanner = new Scanner(stream);
 
-			while (scanner.hasNext()) {
-				String word = scanner.next().toLowerCase();
-				wordList.add(word);
-				String sortedWord = sortWord(word);
+		while (scanner.hasNext()) {
+			String word = scanner.next().toLowerCase();
+			wordList.add(word);
+			String sortedWord = sortWord(word);
 
-				List<String> anagramsOfSortedLetters = anagrams.getOrDefault(sortedWord, new ArrayList<>());
-				anagramsOfSortedLetters.add(word);
-				anagrams.put(sortedWord, anagramsOfSortedLetters);
-			}
-		}
-		catch (Exception e) {
-			System.out.println("Something went wrong. Maybe the file was not found.");
+			List<String> anagramsOfSortedLetters = anagrams.getOrDefault(sortedWord, new ArrayList<>());
+			anagramsOfSortedLetters.add(word);
+			anagrams.put(sortedWord, anagramsOfSortedLetters);
 		}
 	}
 
-	public Map<String, List<String>> getAllAnagramsOfAllWords() {
-		return anagrams;
-	}
-
+	/**
+	* <h2>Find all anagrams of word</h2>
+	* This method returns a list of all anagrams of the given word.
+	*
+	* @param word The word to find anagrams of.
+	* @return {@code List<String>} list of all possible anagrams
+		of the provided word.
+	*/
 	public List<String> getAnagramsOfWord(String word) {
 		word = word.toLowerCase();
 		if (wordList.contains(word)) {
@@ -61,6 +88,14 @@ public class Anagrams {
 		}
 	}
 
+	/**
+	* <h2>Find anagram with specified number</h2>
+	* Returns a random list of anagrams with the length specified.
+	*
+	* @param numberOfAnagrams The length of the list of anagrams to return.
+	* @return {@code List<String>} List of words that are anagrams of each other
+		with the length specified.
+	*/
 	public List<String> getNumberOfAnagrams(int numberOfAnagrams) {
 		List<List <String>> listOfAnagramsWithRightNumber = new ArrayList<>();
 
@@ -81,6 +116,14 @@ public class Anagrams {
 		}
 	}
 
+	/**
+	* <h2>Sort letters alphabetically</h2>
+	* This helper function sorts the letters of a word alphabetically
+	* and returns the result.
+	*
+	* @param word The word to sort alphabetically.
+	* @return {@code String} Letters of {@code word} sorted alphabetically.
+	*/
 	private String sortWord(String word) {
 		char[] charsToSort = word.toCharArray();
 		Arrays.sort(charsToSort);
