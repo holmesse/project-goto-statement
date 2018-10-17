3840628fb4cd08bd493a8450cd7e027b118272a4
David Slusher <davidslusher1@hotmail.com>
2018-10-17 13:52:09 -0400

    adding commented out feature selector in main

--- client/src/main/java/edu/wofford/wordoff/Main.java
+++ client/src/main/java/edu/wofford/wordoff/Main.java
@@ -3,7 +3,38 @@ package edu.wofford.wordoff;
 
 public class Main {
 	public static void main(String[] args) {
-		Feature01Main.main(args);
-		//System.out.println(anagrams.getAnagramsOfWord("sleep"));
+		/*if(args[0] == "1"){
+			Feature01Main.main(args[1]);
+		}
+		else if(args[0] == "2"){
+			Feature02Main.main(args[1]);
+		}
+		else if(args[0] == "3"){
+			Feature03Main.main(args[1]);
+		}
+		else if(args[0] == "4"){
+			Feature04Main.main(args[1]);
+		}
+		else if(args[0] == "5"){
+			Feature05Main.main(args[1]);
+		}
+		else if(args[0] == "6"){
+			Feature06Main.main(args[1]);
+		}
+		else if(args[0] == "7"){
+			Feature07Main.main(args[1]);
+		}
+		else if(args[0] == "8"){
+			Feature08Main.main(args[1]);
+		}
+		else if(args[0] == "9"){
+			Feature09Main.main(args[1]);
+		}
+		else if(args[0] == "10"){
+			Feature10Main.main(args[1]);
+		}
+		else{
+			System.out.println("arrRRRRREEEEEE YOU READY KIDS!!!");
+			*/
+		}
 	}
-}
