c0af2ab969ef8589635c11d17932df4290318710
Aaron Sykes <absykes1997@gmail.com>
2018-10-29 14:15:27 -0400

    Cleaned up Feature04Main code and sprinkled some comments in there

--- client/src/main/java/edu/wofford/wordoff/Feature04Main.java
+++ client/src/main/java/edu/wofford/wordoff/Feature04Main.java
@@ -8,132 +8,72 @@ public class Feature04Main {
     public static void main(String[] args) {
     	if (args.length > 0) {
 
+    		Anagrams anagrams;
+			if (args.length > 1) {
+				long random_seed = Long.parseLong(args[1]);
+				Random randomInstance = new Random(random_seed);
+				anagrams = new Anagrams(randomInstance, "commonwords.txt");
+			} else {
+				anagrams = new Anagrams("commonwords.txt");
+			}
+
+			int difficulty;
+			String chosenWord;
 	    	try {
-	        	int difficulty = Integer.parseInt(args[0]);
+	    		// If argument is an integer, these lines will run.
+	        	difficulty = Integer.parseInt(args[0]);
+			    chosenWord = anagrams.getWordWithLength(difficulty);
+	        } catch (NumberFormatException e) {
+	        	// If the argument is a String, then the error will be caught and this code will run instead.
+	        	chosenWord = args[0];
+	        	difficulty = chosenWord.length();
+	        }
+	        
+	        if (difficulty >= 3 && chosenWord != "") {
 
-		        if (difficulty >= 3) {
+	        	System.out.printf("The word is \"%s\".%n", chosenWord);
 
-			    	Anagrams anagrams;
-					if (args.length > 1) {
-						long random_seed = Long.parseLong(args[1]);
-						Random randomInstance = new Random(random_seed);
-						anagrams = new Anagrams(randomInstance, "commonwords.txt");
-					} else {
-						anagrams = new Anagrams("commonwords.txt");
-					}
-					
-			        String chosenWord = anagrams.getWordWithLength(difficulty);
+		        List<String> subAnagramsOfWord = anagrams.getSubAnagramsOfWord(chosenWord);
+		        subAnagramsOfWord.remove(chosenWord);
 
-			        if (chosenWord != "") {
+			    Scanner reader = new Scanner(System.in);
 
-			        	System.out.printf("The word is \"%s\".%n", chosenWord);
+		        while (subAnagramsOfWord.size() > 0) {
+		        	Map<Integer, List<String>> lengthBuckets = new HashMap<Integer, List<String>>();
+			        for (int i = 0; i < subAnagramsOfWord.size(); i++) {
+			        	String subAnagram = subAnagramsOfWord.get(i);
 
-				        List<String> subAnagramsOfWord = anagrams.getSubAnagramsOfWord(chosenWord);
-				        subAnagramsOfWord.remove(chosenWord);
+			        	List<String> subAnagramsWithSize = lengthBuckets.getOrDefault(subAnagram.length(), new ArrayList<>());
+			        	subAnagramsWithSize.add(subAnagram);
+			        	lengthBuckets.put(subAnagram.length(), subAnagramsWithSize);
+			        }
 
-					    Scanner reader = new Scanner(System.in);
+			        System.out.print("There are ");
 
-				        while (subAnagramsOfWord.size() > 0) {
-				        	Map<Integer, List<String>> lengthBuckets = new HashMap<Integer, List<String>>();
-					        for (int i = 0; i < subAnagramsOfWord.size(); i++) {
-					        	String subAnagram = subAnagramsOfWord.get(i);
+			        for(int len = difficulty; len > 1; len--) {
+			        	int anagramsRemainingWithLength = lengthBuckets.getOrDefault(len, new ArrayList<>()).size();
+			        	if (anagramsRemainingWithLength > 0) {
+			        		System.out.printf("%s [len %s], ", anagramsRemainingWithLength, len);
+			        	} else {
+			        		System.out.printf("0 [len %s], ", len);
+			        	}
+			        }
 
-					        	List<String> subAnagramsWithSize = lengthBuckets.getOrDefault(subAnagram.length(), new ArrayList<>());
-					        	subAnagramsWithSize.add(subAnagram);
-					        	lengthBuckets.put(subAnagram.length(), subAnagramsWithSize);
-					        }
+			        int anagramsRemainingWithLength = lengthBuckets.getOrDefault(1, new ArrayList<>()).size();
+		        	if (anagramsRemainingWithLength > 0) {
+		        		System.out.printf("%s [len 1] anagrams remaining: ", anagramsRemainingWithLength);
+		        	} else {
+		        		System.out.print("0 [len 1] anagrams remaining: ");
+		        	}
 
-					        System.out.print("There are ");
-
-					        for(int len = difficulty; len > 1; len--) {
-					        	int anagramsRemainingWithLength = lengthBuckets.getOrDefault(len, new ArrayList<>()).size();
-					        	if (anagramsRemainingWithLength > 0) {
-					        		System.out.printf("%s [len %s], ", anagramsRemainingWithLength, len);
-					        	} else {
-					        		System.out.printf("0 [len %s], ", len);
-					        	}
-					        }
-
-					        int anagramsRemainingWithLength = lengthBuckets.getOrDefault(1, new ArrayList<>()).size();
-				        	if (anagramsRemainingWithLength > 0) {
-				        		System.out.printf("%s [len 1] anagrams remaining: ", anagramsRemainingWithLength);
-				        	} else {
-				        		System.out.print("0 [len 1] anagrams remaining: ");
-				        	}
-
-					        String guess = reader.next().toLowerCase();
-					        if (subAnagramsOfWord.contains(guess)) {
-					        	subAnagramsOfWord.remove(guess);
-					        }
-				    	}
-				        System.out.println("There are 0 anagrams remaining.");
-			        	reader.close();
-		    		}
-			    }
-
-	    	}
-	    	catch (NumberFormatException e) {
-	    		// Input is string.
-	    		Anagrams anagrams;
-				if (args.length > 1) {
-					long random_seed = Long.parseLong(args[1]);
-					Random randomInstance = new Random(random_seed);
-					anagrams = new Anagrams(randomInstance, "commonwords.txt");
-				} else {
-					anagrams = new Anagrams("commonwords.txt");
-				}
-				
-		        String chosenWord = args[0];
-
-		        if (chosenWord != "") {
-
-			        List<String> subAnagramsOfWord = anagrams.getSubAnagramsOfWord(chosenWord);
-			        subAnagramsOfWord.remove(chosenWord);
-
-			        if (subAnagramsOfWord.size() > 0) {
-
-			        	System.out.printf("The word is \"%s\".%n", chosenWord);
-
-					    Scanner reader = new Scanner(System.in);
-
-				        while (subAnagramsOfWord.size() > 0) {
-				        	Map<Integer, List<String>> lengthBuckets = new HashMap<Integer, List<String>>();
-					        for (int i = 0; i < subAnagramsOfWord.size(); i++) {
-					        	String subAnagram = subAnagramsOfWord.get(i);
-
-					        	List<String> subAnagramsWithSize = lengthBuckets.getOrDefault(subAnagram.length(), new ArrayList<>());
-					        	subAnagramsWithSize.add(subAnagram);
-					        	lengthBuckets.put(subAnagram.length(), subAnagramsWithSize);
-					        }
-
-					        System.out.print("There are ");
-
-					        for(int len = chosenWord.length(); len > 1; len--) {
-					        	int anagramsRemainingWithLength = lengthBuckets.getOrDefault(len, new ArrayList<>()).size();
-					        	if (anagramsRemainingWithLength > 0) {
-					        		System.out.printf("%s [len %s], ", anagramsRemainingWithLength, len);
-					        	} else {
-					        		System.out.printf("0 [len %s], ", len);
-					        	}
-					        }
-
-					        int anagramsRemainingWithLength = lengthBuckets.getOrDefault(1, new ArrayList<>()).size();
-				        	if (anagramsRemainingWithLength > 0) {
-				        		System.out.printf("%s [len 1] anagrams remaining: ", anagramsRemainingWithLength);
-				        	} else {
-				        		System.out.print("0 [len 1] anagrams remaining: ");
-				        	}
-
-					        String guess = reader.next().toLowerCase();
-					        if (subAnagramsOfWord.contains(guess)) {
-					        	subAnagramsOfWord.remove(guess);
-					        }
-				        	System.out.println("There are 0 anagrams remaining.");
-				        }
-		        		reader.close();
-			    	}
-			    }
-	    	}
+			        String guess = reader.next().toLowerCase();
+			        if (subAnagramsOfWord.contains(guess)) {
+			        	subAnagramsOfWord.remove(guess);
+			        }
+		    	}
+		        System.out.println("There are 0 anagrams remaining.");
+	        	reader.close();
+		    }
     	}
     }
 }
