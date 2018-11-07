eeecb08cdc8255e01f1d0decbe4d04177aa4ef76
Aaron Sykes <absykes1997@gmail.com>
2018-11-07 15:00:38 -0500

    Finished implementation of AnagramsController.

--- service/src/main/java/edu/wofford/wordoff/service/AnagramsController.java
+++ service/src/main/java/edu/wofford/wordoff/service/AnagramsController.java
@@ -1,10 +1,17 @@
 package edu.wofford.wordoff.service;
 
-import java.util.concurrent.atomic.AtomicLong;
+import edu.wofford.wordoff.*;
+import java.util.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AnagramsController {
+
+	private Anagrams anagrams;
+
+	private void initAnagramsWithSource(String source) {
+		anagrams = new Anagrams(source + "words.txt");
+	}
 
 	/**
 	* Finds the number of words listed in the specified source.
@@ -16,17 +23,101 @@ public class AnagramsController {
 	*/
 	@RequestMapping("/wordoff/{source}/numwords")
 	public int getNumberOfWordsInSource(@PathVariable String source) {
-		String fileName;
-		if (source.equals("all")) {
-			fileName = "allwords.txt";
-		} else if (source.equals("common")) {
-			fileName = "commonwords.txt";
-		} else {
-			fileName = "";
-			System.err.println("Unrecognized source.");
-			System.exit(0);
-		}
-		Anagrams anagrams = new Anagrams(fileName);
+		initAnagramsWithSource(source);
 		return anagrams.getNumberOfWords();
+	}
+
+	/**
+	* Checks if the provided word is valid in the given source.
+	*
+	* @param source "all" or "common". The source that contains
+	* the words that are considered valid for anagram purposes.
+	* @param word The word to check for validity.
+	* @return {@code boolean} True if word is valid, false otherwise.
+	*/
+	@RequestMapping("/wordoff/{source}/isword/{word}")
+	public boolean wordIsInSource(@PathVariable String source, @PathVariable String word) {
+		initAnagramsWithSource(source);
+		return anagrams.isWord(word);
+	}
+
+	/**
+	* Finds a random word with the specified length.
+	*
+	* @param source "all" or "common". The source that contains
+	* the words that are considered valid for anagram purposes.
+	* @param length The length of a word to find.
+	* @return {@code String} A word with the provided length.
+	*/
+	@RequestMapping("/wordoff/{source}/wordwithlength/{length}")
+	public String findWordWithLength(@PathVariable String source, @PathVariable int length) {
+		initAnagramsWithSource(source);
+		return anagrams.getWordWithLength(length);
+	}
+
+	/**
+	* Finds a random word that has the provided number of anagrams.
+	*
+	* @param source "all" or "common". The source that contains
+	* the words that are considered valid for anagram purposes.
+	* @param numanagrams The number of anagrams the returned word
+	* will have.
+	* @return {@code String} A word that has the provided number
+	* of anagrams.
+	*/
+	@RequestMapping("/wordoff/{source}/wordwithanagrams/{numanagrams}")
+	public String findWordWithAnagrams(@PathVariable String source, @PathVariable int numanagrams) {
+		initAnagramsWithSource(source);
+		return anagrams.getWordWithNumberOfAnagrams(numanagrams);
+	}
+
+	/**
+	* Finds all anagrams of the given word.
+	*
+	* @param source "all" or "common". The source that contains
+	* the words that are considered valid for anagram purposes.
+	* @param word The word to find anagrams of.
+	* @return {@code List<String>} A JSON array of anagrams of word.
+	*/
+	@RequestMapping("/wordoff/{source}/anagrams/{word}")
+	public List<String> findAnagramsOfWord(@PathVariable String source, @PathVariable String word) {
+		initAnagramsWithSource(source);
+		return anagrams.getAnagramsOfWord(word);
+	}
+
+	/**
+	* Finds all sub-anagrams of the given word.
+	*
+	* @param source "all" or "common". The source that contains
+	* the words that are considered valid for anagram purposes.
+	* @param word The word to find sub-anagrams of.
+	* @return {@code List<String>} A JSON array of sub-anagrams of word.
+	*/
+	@RequestMapping("/wordoff/{source}/subanagrams/{word}")
+	public List<String> findSubAnagramsOfWord(@PathVariable String source, @PathVariable String word) {
+		// Acceptance tests say that this should not include the full anagrams of the word.
+		// If that changes, simply replace this code with the code in findAllAnagramsOfWord.
+		initAnagramsWithSource(source);
+		List<String> subAnagrams = anagrams.getSubAnagramsOfWord(word);
+		for (int i = 0; i < subAnagrams.size(); i++) {
+			if (subAnagrams.get(i).length() == word.length()) {
+				subAnagrams.remove(i);
+			}
+		}
+		return subAnagrams;
+	}
+
+	/**
+	* Finds all anagrams of the given word.
+	*
+	* @param source "all" or "common". The source that contains
+	* the words that are considered valid for anagram purposes.
+	* @param word The word to find full and sub anagrams of.
+	* @return {@code List<String>} A JSON array of all anagrams of word.
+	*/
+	@RequestMapping("/wordoff/{source}/allanagrams/{word}")
+	public List<String> findAllAnagramsOfWord(@PathVariable String source, @PathVariable String word) {
+		initAnagramsWithSource(source);
+		return anagrams.getSubAnagramsOfWord(word);
 	}
 }
