46c97d5809d888229530630066315b49bad29bdb
Aaron Sykes <absykes1997@gmail.com>
2018-11-14 15:06:30 -0500

    Renamed database file and added documentation.

--- client/src/main/java/edu/wofford/wordoff/AnagramsLeaderboard.java
+++ client/src/main/java/edu/wofford/wordoff/AnagramsLeaderboard.java
@@ -6,12 +6,17 @@ import java.sql.*;
 * This class sets up the results.db SQLite database that is used for keeping track
 * of game results and high scores.
 */
-public class SQLiteJDBC {
+public class AnagramsLeaderboard {
 
    public static void main( String args[] ) {
-      
    }
 
+   /**
+   * Create Leaderboard.
+   * This method creates a new leaderboard table if one
+   * does not already exist. If the leaderboard table
+   * exists, nothing happens.
+   */
    public void createLeaderboardTable() {
       Connection conn = null;
       Statement stmt = null;
@@ -22,7 +27,7 @@ public class SQLiteJDBC {
          System.out.println("Opened database successfully");
 
          stmt = conn.createStatement();
-         String createLeaderboardSQL = "CREATE TABLE leaderboard (" +
+         String createLeaderboardSQL = "CREATE TABLE IF NOT EXISTS leaderboard (" +
                         " word TEXT NOT NULL, " + 
                         " difficulty INT NOT NULL, " + 
                         " seconds_left INT NOT NULL, " + 
@@ -38,6 +43,15 @@ public class SQLiteJDBC {
       System.out.println("Table created successfully");
    }
 
+   /**
+   * Insert new result into the leaderboard.
+   * Inserts a new record into the leaderboard table with the
+   * specified values of word, difficulty, and seconds_left.
+   *
+   * @param word The word played.
+   * @param difficulty The difficulty of the game.
+   * @param seconds_left The time remaining after winning.
+   */
    public void insertNewResult(String word, int difficulty, int seconds_left) {
       Connection conn = null;
       Statement stmt = null;
@@ -45,14 +59,19 @@ public class SQLiteJDBC {
       try {
          Class.forName("org.sqlite.JDBC");
          conn = DriverManager.getConnection("jdbc:sqlite:client/build/libs/results.db");
-         System.out.println("Opened database successfully");
+         System.out.println(String.format("Inserting %s", word));
 
          stmt = conn.createStatement();
-         String insertSQL = String.format("INSERT INTO leaderboard (word, difficulty, seconds_left) VALUES (%s, %d, %d);", word, difficulty, seconds_left);
+         String insertSQL = String.format("INSERT INTO leaderboard (word, difficulty, seconds_left) VALUES ('%s', %d, %d);", word, difficulty, seconds_left);
 
          stmt.executeUpdate(insertSQL);
          stmt.close();
          conn.close();  
+      } catch (SQLException e) {
+         if (e.getErrorCode() != 19) { //19 is the error code for the UNIQUE constraint violation, which we want to ignore.
+            System.err.println("Error code (" + e.getErrorCode() + "): " + e.getMessage());
+            System.exit(0);
+         }
       } catch (Exception e) {
          System.err.println(e.getClass().getName() + ": " + e.getMessage());
          System.exit(0);
@@ -60,6 +79,15 @@ public class SQLiteJDBC {
       System.out.println("Insert executed successfully");
    }
 
+   /**
+   * Get results from the leaderboard.
+   * Gets the top 5 results from the leaderboard and returns
+   * them as a 2-dimensional String array. Columns are, in order:
+   * rank, word, difficulty, seconds_left
+   *
+   * @return {@code String[][]} Two-dimensional string array
+   * representing the results of the query.
+   */
    public String[][] selectLeaderboardData() {
       Connection conn = null;
       Statement stmt = null;
@@ -69,10 +97,10 @@ public class SQLiteJDBC {
       try {
          Class.forName("org.sqlite.JDBC");
          conn = DriverManager.getConnection("jdbc:sqlite:client/build/libs/results.db");
-         System.out.println("Opened database successfully");
+         System.out.println("Selecting data");
 
          stmt = conn.createStatement();
-         String insertSQL = "SELECT [word] AS 'Word', [difficulty] AS 'Difficulty', [seconds_left] AS 'Seconds Left' FROM leaderboard ORDER BY (([difficulty] * 10 - [seconds_left]) / [difficulty]) LIMIT 5;";
+         String insertSQL = "SELECT [word], [difficulty], [seconds_left] FROM leaderboard ORDER BY (([difficulty] * 10 - [seconds_left]) / [difficulty]) LIMIT 5;";
 
          ResultSet rs = stmt.executeQuery(insertSQL);
 
@@ -80,6 +108,7 @@ public class SQLiteJDBC {
          while (rs.next()) {
             String[] row = {String.valueOf(rank), rs.getString("word"), String.valueOf(rs.getInt("difficulty")), String.valueOf(rs.getInt("seconds_left"))};
             results[rank - 1] = row;
+            rank++;
          }
 
          rs.close();
--- client/src/main/java/edu/wofford/wordoff/Feature07Main.java
+++ client/src/main/java/edu/wofford/wordoff/Feature07Main.java
@@ -3,7 +3,23 @@ package edu.wofford.wordoff;
 public class Feature07Main {
 
 	public static void main(String[] args) {
-		SQLiteJDBC.main(null);
+		AnagramsLeaderboard leaderboard = new AnagramsLeaderboard();
+
+		leaderboard.createLeaderboardTable();
+
+		leaderboard.insertNewResult("apple", 2, 8);
+		leaderboard.insertNewResult("hello", 3, 18);
+		leaderboard.insertNewResult("cat", 2, 6);
+		leaderboard.insertNewResult("silly", 6, 12);
+		leaderboard.insertNewResult("world", 4, 20);
+
+		String[][] results = leaderboard.selectLeaderboardData();
+		for (String[] row : results) {
+			for (String item : row) {
+				System.out.print(item + ", ");
+			}
+			System.out.println();
+		}
 	}
 	
 }
