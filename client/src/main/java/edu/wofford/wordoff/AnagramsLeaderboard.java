96527ca9c92aec2a81170ed136e4c560a6835513
Aaron Sykes <absykes1997@gmail.com>
2018-11-28 15:11:57 -0500

    pushing up unit tests for the leaderboard, as well as the changes needed to the files that use it since I changed the error handling.

--- client/src/main/java/edu/wofford/wordoff/AnagramsLeaderboard.java
+++ client/src/main/java/edu/wofford/wordoff/AnagramsLeaderboard.java
@@ -9,6 +9,8 @@ import java.util.*;
 */
 public class AnagramsLeaderboard {
 
+   private String pathToDatabase = "jdbc:sqlite:results.db";
+
    public static void main( String args[] ) {
    }
 
@@ -17,29 +19,26 @@ public class AnagramsLeaderboard {
    * This method creates a new leaderboard table if one
    * does not already exist. If the leaderboard table
    * exists, nothing happens.
+   *
+   * @throws ClassNotFoundException When sqlite JDBC cannot be found.
+   * @throws SQLException If an error occurs when creating the leaderboard table.
    */
-   public void createLeaderboardTable() {
+   public void createLeaderboardTable() throws ClassNotFoundException, SQLException {
       Connection conn = null;
       Statement stmt = null;
-      
-      try {
-         Class.forName("org.sqlite.JDBC");
-         conn = DriverManager.getConnection("jdbc:sqlite:client/build/libs/results.db");
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection(pathToDatabase);
 
-         stmt = conn.createStatement();
-         String createLeaderboardSQL = "CREATE TABLE IF NOT EXISTS leaderboard (" +
-                        " word TEXT NOT NULL, " + 
-                        " difficulty INT NOT NULL, " + 
-                        " seconds_left INT NOT NULL, " + 
-                        " submitDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
-                        " PRIMARY KEY (word, difficulty));"; 
-         stmt.executeUpdate(createLeaderboardSQL);
-         stmt.close();
-         conn.close();
-      } catch (Exception e) {
-         System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
-         System.exit(0);
-      }
+      stmt = conn.createStatement();
+      String createLeaderboardSQL = "CREATE TABLE IF NOT EXISTS leaderboard (" +
+                     " word TEXT NOT NULL, " + 
+                     " difficulty INT NOT NULL, " + 
+                     " seconds_left INT NOT NULL, " + 
+                     " submitDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
+                     " PRIMARY KEY (word, difficulty));"; 
+      stmt.executeUpdate(createLeaderboardSQL);
+      stmt.close();
+      conn.close();
    }
 
    /**
@@ -50,28 +49,24 @@ public class AnagramsLeaderboard {
    * @param word The word played.
    * @param difficulty The difficulty of the game.
    * @param seconds_left The time remaining after winning.
+   *
+   * @throws ClassNotFoundException When sqlite JDBC cannot be found.
+   * @throws SQLException If an error occurs when inserting data
+   * into the leaderboard table.
    */
-   public void insertNewResult(String word, int difficulty, int seconds_left) {
+   public void insertNewResult(String word, int difficulty, int seconds_left) throws ClassNotFoundException, SQLException {
       Connection conn = null;
       Statement stmt = null;
 
-      try {
-         Class.forName("org.sqlite.JDBC");
-         conn = DriverManager.getConnection("jdbc:sqlite:client/build/libs/results.db");
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection(pathToDatabase);
 
-         stmt = conn.createStatement();
-         String insertSQL = String.format("INSERT OR IGNORE INTO leaderboard (word, difficulty, seconds_left) VALUES ('%s', %d, %d);", word, difficulty, seconds_left);
+      stmt = conn.createStatement();
+      String insertSQL = String.format("INSERT OR IGNORE INTO leaderboard (word, difficulty, seconds_left) VALUES ('%s', %d, %d);", word, difficulty, seconds_left);
 
-         stmt.executeUpdate(insertSQL);
-         stmt.close();
-         conn.close();  
-      } catch (SQLException e) {
-         System.err.println("ERROR: Error code (" + e.getErrorCode() + "): " + e.getMessage());
-         System.exit(0);
-      } catch (Exception e) {
-         System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
-         System.exit(0);
-      }
+      stmt.executeUpdate(insertSQL);
+      stmt.close();
+      conn.close();
    }
 
    /**
@@ -82,51 +77,50 @@ public class AnagramsLeaderboard {
    *
    * @return {@code String[][]} Two-dimensional string array
    * representing the results of the query.
+   *
+   * @throws ClassNotFoundException When sqlite JDBC cannot be found.
+   * @throws SQLException If an error occurs when inserting data
+   * into the leaderboard table.
    */
-   public String[][] selectLeaderboardData() {
+   public String[][] selectLeaderboardData() throws ClassNotFoundException, SQLException {
       Connection conn = null;
       Statement stmt = null;
 
       String[][] results = new String[5][4];
       String[][] leaderboardData = new String[0][0];
 
-      try {
-         Class.forName("org.sqlite.JDBC");
-         conn = DriverManager.getConnection("jdbc:sqlite:client/build/libs/results.db");
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection(pathToDatabase);
 
-         stmt = conn.createStatement();
-         String insertSQL = "SELECT [word], [difficulty], [seconds_left] FROM leaderboard ORDER BY (([difficulty] * 10 - [seconds_left]) / [difficulty]) LIMIT 5;";
+      stmt = conn.createStatement();
+      String insertSQL = "SELECT [word], [difficulty], [seconds_left] FROM leaderboard ORDER BY (([difficulty] * 10 - [seconds_left]) / [difficulty]) LIMIT 5;";
 
-         ResultSet rs = stmt.executeQuery(insertSQL);
+      ResultSet rs = stmt.executeQuery(insertSQL);
 
-         int rank = 1;
-         while (rs.next()) {
-            String[] row = {String.valueOf(rank), rs.getString("word"), String.valueOf(rs.getInt("difficulty")), String.valueOf(rs.getInt("seconds_left"))};
-            results[rank - 1] = row;
-            rank++;
-         }
-
-         int rowCounter = 0;
-         for (int i = 0; i < 5; i++) {
-            if (results[i][0] != null) {
-               rowCounter++;
-            }
-         }
-
-         leaderboardData = new String[rowCounter][4];
-         for (int j = 0; j < 5; j++) {
-            if (results[j][0] != null) {
-               leaderboardData[j] = results[j];
-            }
-         }
-
-         rs.close();
-         stmt.close();
-         conn.close();  
-      } catch (Exception e) {
-         System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
-         System.exit(0);
+      int rank = 1;
+      while (rs.next()) {
+         String[] row = {String.valueOf(rank), rs.getString("word"), String.valueOf(rs.getInt("difficulty")), String.valueOf(rs.getInt("seconds_left"))};
+         results[rank - 1] = row;
+         rank++;
       }
+
+      int rowCounter = 0;
+      for (int i = 0; i < 5; i++) {
+         if (results[i][0] != null) {
+            rowCounter++;
+         }
+      }
+
+      leaderboardData = new String[rowCounter][4];
+      for (int j = 0; j < 5; j++) {
+         if (results[j][0] != null) {
+            leaderboardData[j] = results[j];
+         }
+      }
+
+      rs.close();
+      stmt.close();
+      conn.close();
       return leaderboardData;
    }
 
@@ -135,63 +129,61 @@ public class AnagramsLeaderboard {
    * Determines whether the leaderboard table has rows or not.
    *
    * @return {@code boolean} true if table is empty, false otherwise.
+   *
+   * @throws ClassNotFoundException When sqlite JDBC cannot be found.
+   * @throws SQLException If an error occurs when checking if the table
+   * is empty.
    */
-   public boolean leaderboardIsEmpty() {
+   public boolean leaderboardIsEmpty() throws ClassNotFoundException, SQLException {
       Connection conn = null;
       Statement stmt = null;
 
       boolean isEmpty = false;
 
-      try {
-         Class.forName("org.sqlite.JDBC");
-         conn = DriverManager.getConnection("jdbc:sqlite:client/build/libs/results.db");
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection(pathToDatabase);
 
-         stmt = conn.createStatement();
-         String insertSQL = "SELECT COUNT(*) AS 'count' FROM leaderboard;";
+      stmt = conn.createStatement();
+      String insertSQL = "SELECT COUNT(*) AS 'count' FROM leaderboard;";
 
-         ResultSet rs = stmt.executeQuery(insertSQL);
+      ResultSet rs = stmt.executeQuery(insertSQL);
 
-         while (rs.next()) {
-            if (rs.getInt("count") == 0) {
-               isEmpty = true;
-            } else {
-               isEmpty = false;
-            }
+      while (rs.next()) {
+         if (rs.getInt("count") == 0) {
+            isEmpty = true;
+         } else {
+            isEmpty = false;
          }
-
-         rs.close();
-         stmt.close();
-         conn.close();  
-      } catch (Exception e) {
-         System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
-         System.exit(0);
       }
+
+      rs.close();
+      stmt.close();
+      conn.close();
       return isEmpty;
    }
 
    /**
    * Clears leaderboard of all data.
    * Deletes all rows from the leaderboard table.
+   *
+   * @throws ClassNotFoundException When sqlite JDBC cannot be found.
+   * @throws SQLException If an error occurs when clearing the data
+   * from the leaderboard.
    */
-   public void clearLeaderboardData() {
+   public void clearLeaderboardData() throws ClassNotFoundException, SQLException {
       Connection conn = null;
       Statement stmt = null;
 
-      try {
-         Class.forName("org.sqlite.JDBC");
-         conn = DriverManager.getConnection("jdbc:sqlite:client/build/libs/results.db");
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection(pathToDatabase);
 
-         stmt = conn.createStatement();
-         String deleteCmd = "DELETE FROM leaderboard;";
+      stmt = conn.createStatement();
+      String deleteCmd = "DELETE FROM leaderboard;";
 
-         stmt.executeUpdate(deleteCmd);
+      stmt.executeUpdate(deleteCmd);
 
-         stmt.close();
-         conn.close();  
-      } catch (Exception e) {
-         System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
-         System.exit(0);
-      }
+      stmt.close();
+      conn.close();
    }
 
 }
--- client/src/main/java/edu/wofford/wordoff/LeaderboardDialog.java
+++ client/src/main/java/edu/wofford/wordoff/LeaderboardDialog.java
@@ -15,8 +15,8 @@ import javax.swing.table.*;
 import javax.swing.table.AbstractTableModel;
 
 public class LeaderboardDialog extends JDialog{
-  private JDialog leaderboardDialog;
-  private AnagramsLeaderboard leaderboardData;
+    private JDialog leaderboardDialog;
+    private AnagramsLeaderboard leaderboardData;
 
 /**
 * Generates an new modeless dialog box with a {@code JTable} that shows the
@@ -27,32 +27,61 @@ public class LeaderboardDialog extends JDialog{
 * @param difficulty The number of anagams needed to be found.
 * @param time The remaining time after all anagrams were found.
 */
-  public LeaderboardDialog(JFrame parent, String selectedWord, int difficulty, int time) {
-    leaderboardData = new AnagramsLeaderboard();
+    public LeaderboardDialog(JFrame parent, String selectedWord, int difficulty, int time) {
+        leaderboardData = new AnagramsLeaderboard();
 
-    leaderboardData.createLeaderboardTable();
-    if(!leaderboardData.leaderboardIsEmpty()) {
-      leaderboardDialog = new JDialog(parent);
-      leaderboardDialog.add(Box.createRigidArea(new Dimension(500, 300)));
+        try {
+            leaderboardData.createLeaderboardTable();
+        } catch (Exception e) {
+            System.err.println("Leaderboard table was not created due to an error. See stack trace for details.");
+            System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
+            e.printStackTrace();
+        }
 
-      if(time > 0) {
-        leaderboardData.insertNewResult(selectedWord, difficulty, time);
-      }
+        boolean tableIsEmpty = true;
+        try {
+            tableIsEmpty = leaderboardData.leaderboardIsEmpty();
+        } catch (Exception e) {
+            System.err.println("Exception raised when checking to see if the leaderboard was empty. See stack trace for details.");
+            System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
+            e.printStackTrace();
+        }
 
-      String[][] dataModel = leaderboardData.selectLeaderboardData();
+        if (!tableIsEmpty) {
+            leaderboardDialog = new JDialog(parent);
+            leaderboardDialog.add(Box.createRigidArea(new Dimension(500, 300)));
 
-      String[] columnNames = {"Rank", "Word", "Difficulty", "Seconds Left"};
+            if (time > 0) {
+                try {
+                    leaderboardData.insertNewResult(selectedWord, difficulty, time); 
+                } catch (Exception e) {
+                    System.err.println("Data was not successfully inserted into leaderboard."
+                        + "See stack trace for details.");
+                    System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
 
-      JTable leaderboard = new JTable(dataModel, columnNames);
-      leaderboard.setRowHeight(30);
-      leaderboardDialog.add(new JScrollPane(leaderboard));
+            try {
+                String[][] dataModel = leaderboardData.selectLeaderboardData();
 
-      leaderboard.setName("leaderboard");
-      leaderboardDialog.setTitle("Leaderboard");
-      leaderboardDialog.pack();
-      leaderboardDialog.setVisible(true);
+                String[] columnNames = {"Rank", "Word", "Difficulty", "Seconds Left"};
+
+                JTable leaderboard = new JTable(dataModel, columnNames);
+                leaderboard.setRowHeight(30);
+                leaderboardDialog.add(new JScrollPane(leaderboard));
+
+                leaderboard.setName("leaderboard");
+                leaderboardDialog.setTitle("Leaderboard");
+                leaderboardDialog.pack();
+                leaderboardDialog.setVisible(true);
+            } catch (Exception e) {
+                System.err.println("An exception was raised when selecting data. See stack trace for details.");
+                System.err.println("ERROR: " + e.getClass().getName() + ": " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
     }
-
-  }
 
 }
